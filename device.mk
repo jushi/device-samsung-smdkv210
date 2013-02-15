c3709a8e0c0e931125f67210a8b6cd4ae8b609fd
Andrey Kozlov <Andrey.Kozlov@teleca.com>
2013-02-15 17:26:16 +0400

    Update root files

--- device.mk
+++ device.mk
@@ -71,12 +71,12 @@ PRODUCT_COPY_FILES += \
 
 # Busybox + scripts
 PRODUCT_COPY_FILES += \
-	device/samsung/smdkv210/su:system/bin/su \
-	device/samsung/smdkv210/busybox:system/xbin/busybox \
-	device/samsung/smdkv210/insmod.sh:system/etc/insmod.sh \
-	device/samsung/smdkv210/check_property.sh:system/etc/check_property.sh \
-	device/samsung/smdkv210/install_busybox.sh:system/etc/install_busybox.sh \
-	device/samsung/smdkv210/initial_setup.sh:system/etc/initial_setup.sh
+	device/samsung/smdkv210/rooting/su:system/bin/su \
+	device/samsung/smdkv210/rooting/busybox:system/xbin/busybox \
+	device/samsung/smdkv210/rooting/insmod.sh:system/etc/insmod.sh \
+	device/samsung/smdkv210/rooting/check_property.sh:system/etc/check_property.sh \
+	device/samsung/smdkv210/rooting/install_busybox.sh:system/etc/install_busybox.sh \
+	device/samsung/smdkv210/rooting/initial_setup.sh:system/etc/initial_setup.sh
 
 # Bluetooth setup
 PRODUCT_COPY_FILES += \
@@ -174,8 +174,8 @@ PRODUCT_PACKAGES += \
 
 # GPS config
 PRODUCT_COPY_FILES += \
-	device/samsung/smdkv210/gps.xml:system/vendor/etc/gps.xml \
-	device/samsung/smdkv210/gps.conf:system/etc/gps.conf
+	device/samsung/smdkv210/gps/gps.xml:system/vendor/etc/gps.xml \
+	device/samsung/smdkv210/gps/gps.conf:system/etc/gps.conf
 
 # These are the OpenMAX IL configuration files
 PRODUCT_COPY_FILES += \
